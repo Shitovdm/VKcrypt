3975fc53fd78ea6f7f09b5901ad95400c260d8b2
Shitov.dm <shitov.dm@gmail.com>
2014-06-19 13:44:34 +0300

    Добавлена внешняя библиотека для работы с большими числами.

--- background.js
+++ background.js
@@ -1,4 +1,11 @@
 
+/*
+ *
+ * 
+ *   
+ */
+
+
 $(document).ready(function () {
     $.get(chrome.extension.getURL('js/inject.js'),
         function (data) {
@@ -16,9 +23,22 @@ $(document).ready(function () {
             document.getElementsByTagName("head")[0].appendChild(script);
             document.getElementsByTagName("body")[0].setAttribute("onLoad", "onPageLoad();");
             document.getElementsByTagName("body")[0].setAttribute("onhashchange", "onhashchange();");
-            
         }
     );
+    
+    //  BigInteger.
+    $.get(chrome.extension.getURL('js/external/BigInteger.min.js'),
+        function (data) {
+            //  Подключаем стили.
+            var script = document.createElement("script");
+            script.setAttribute("type", "text/javascript");
+            script.innerHTML = data;
+            document.getElementsByTagName("head")[0].appendChild(script);
+            //document.getElementsByTagName("body")[0].setAttribute("onLoad", "onPageLoad();");
+            //document.getElementsByTagName("body")[0].setAttribute("onhashchange", "onhashchange();");
+        }
+    );
+    
 });
 
 
